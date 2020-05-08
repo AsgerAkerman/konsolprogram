b9347fd7bbec266119f65f1a69a19cf48e262143
AsgerAkerman <43748689+AsgerAkerman@users.noreply.github.com>
2020-05-08 10:59:39 +0200

    format

--- src/com/company/RestApiClient.java
+++ src/com/company/RestApiClient.java
@@ -35,33 +35,33 @@ public class RestApiClient {
         Calendar cal = Calendar.getInstance();
         cal.setTime(today);
 
-            while(adgangNægtet) {
+        while (adgangNægtet) {
             System.out.println("Bruger:");
             bruger = scan.next();
             System.out.println("Adgangskode:");
             kodeord = scan.next();
 
-            try{
-                if(login(bruger, kodeord)) {
+            try {
+                if (login(bruger, kodeord)) {
                     adgangNægtet = false;
                     System.out.println("Bruger godkendt");
                 }
-            } catch (Exception e){
+            } catch (Exception e) {
                 //e.printStackTrace();
                 System.out.println("Bruger ikke godkendt" + "Prøv igen.");
                 System.out.println("-------------------------");
             }
         }
-        while(!doneWithProgram) {
+        while (!doneWithProgram) {
             int brugerValg = mainMenu();
 
             switch (brugerValg) {
                 case 1:
-                    boolean valg1 = false ;
-                    while(!valg1){
+                    boolean valg1 = false;
+                    while (!valg1) {
                         System.out.println("               ");
                         System.out.println("Følgende bookings er optaget idag:");
-                        System.out.println(getBookingsOfDate(cal.get(Calendar.DAY_OF_MONTH),cal.get(Calendar.MONTH)+1,cal.get(Calendar.YEAR),bruger,kodeord));
+                        System.out.println(getBookingsOfDate(cal.get(Calendar.DAY_OF_MONTH), cal.get(Calendar.MONTH) + 1, cal.get(Calendar.YEAR), bruger, kodeord));
                         System.out.println("Ønsker du at booke et lokale idag? Så indtast følgende oplysninger:");
                         System.out.println("-------------------------");
                         System.out.println("1 for 08:00-12:00");
@@ -74,7 +74,7 @@ public class RestApiClient {
                         int roomId = scan.nextInt();
                         System.out.println("-------------------------");
                         System.out.println("Laver din booking...");
-                        createBooking(timeblock,roomId,getUserid(bruger,kodeord),bruger,cal.get(Calendar.DAY_OF_MONTH),cal.get(Calendar.MONTH),cal.get(Calendar.YEAR),bruger,kodeord);
+                        createBooking(timeblock, roomId, getUserid(bruger, kodeord), bruger, cal.get(Calendar.DAY_OF_MONTH), cal.get(Calendar.MONTH), cal.get(Calendar.YEAR), bruger, kodeord);
                         System.out.println("Din booking er blevet lavet.");
                         valg1 = true;
                     }
@@ -82,13 +82,13 @@ public class RestApiClient {
                 case 2:
                     System.out.println("Finder dine bookings...");
                     System.out.println("--------------------------------------------------------------------------------------------------");
-                    System.out.println(getBookings(getUserid(bruger,kodeord),bruger,kodeord));
+                    System.out.println(getBookings(getUserid(bruger, kodeord), bruger, kodeord));
 
                     break;
                 case 3:
                     System.out.println("Finder dine oplysninger...");
                     System.out.println("-------------------------");
-                    System.out.println("Userid : " + getUserid(bruger,kodeord));
+                    System.out.println("Userid : " + getUserid(bruger, kodeord));
                     System.out.println("Brugernavn : " + bruger);
                     break;
                 case 4:
@@ -101,7 +101,7 @@ public class RestApiClient {
         }
     }
 
-    public static int mainMenu(){
+    public static int mainMenu() {
         int valg;
         Scanner sc = new Scanner(System.in);
         System.out.println("-------------------------");
@@ -117,53 +117,46 @@ public class RestApiClient {
 
     public static boolean login(String username, String password) throws IOException, JSONException {
         try (final CloseableHttpClient httpclient = HttpClients.createDefault()) {
-        final HttpPost post = new HttpPost("http://ec2-3-21-232-61.us-east-2.compute.amazonaws.com:8081/login");
+            final HttpPost post = new HttpPost("http://ec2-3-21-232-61.us-east-2.compute.amazonaws.com:8081/login");
 
-        JSONObject payload = new JSONObject();
-        payload.put("username", username);
-        payload.put("password", password);
-        post.setEntity(new StringEntity(payload.toString(), ContentType.APPLICATION_JSON));
-
-
-
-        // En response handler taget fra Apaches side: https://hc.apache.org/httpcomponents-client-5.0.x/httpclient5/examples/ClientWithResponseHandler.java
-        final HttpClientResponseHandler<String> responseHandler = response -> {
-            final int status = response.getCode();
-            if (status >= HttpStatus.SC_SUCCESS && status < HttpStatus.SC_REDIRECTION) {
-                final HttpEntity entity = response.getEntity();
-                try {
-                    return entity != null ? EntityUtils.toString(entity) : null;
-                } catch (final ParseException ex) {
-                    throw new ClientProtocolException(ex);
+            JSONObject payload = new JSONObject();
+            payload.put("username", username);
+            payload.put("password", password);
+            post.setEntity(new StringEntity(payload.toString(), ContentType.APPLICATION_JSON));
+            // En response handler taget fra Apaches side: https://hc.apache.org/httpcomponents-client-5.0.x/httpclient5/examples/ClientWithResponseHandler.java
+            final HttpClientResponseHandler<String> responseHandler = response -> {
+                final int status = response.getCode();
+                if (status >= HttpStatus.SC_SUCCESS && status < HttpStatus.SC_REDIRECTION) {
+                    final HttpEntity entity = response.getEntity();
+                    try {
+                        return entity != null ? EntityUtils.toString(entity) : null;
+                    } catch (final ParseException ex) {
+                        throw new ClientProtocolException(ex);
+                    }
+                } else {
+                    throw new ClientProtocolException("Unexpected response status: " + status);
                 }
-            } else {
-                throw new ClientProtocolException("Unexpected response status: " + status);
-            }
-
-        };
-        final String responseBody = httpclient.execute(post, responseHandler);
-        System.out.println("-------------------------");
-            if(responseBody.equals("true")) {
+            };
+            final String responseBody = httpclient.execute(post, responseHandler);
+            System.out.println("-------------------------");
+            if (responseBody.equals("true")) {
                 return true;
-
-            } else{
+            } else {
                 System.out.println("false");
                 return false;
             }
         }
-   }
-    public static String getBookingsOfDate(int day,int month, int year, String username, String password) throws IOException, JSONException, ParseException, URISyntaxException {
+    }
+
+    public static String getBookingsOfDate(int day, int month, int year, String username, String password) throws IOException, JSONException, ParseException, URISyntaxException {
 
         try (final CloseableHttpClient httpclient = HttpClients.createDefault()) {
-            final HttpGet get = new HttpGet("http://ec2-3-21-232-61.us-east-2.compute.amazonaws.com:8081/bookings/findByDate/"+ day + "/" + month + "/"+ year);
-            //+ day + "/" + month + "/"+ year
+            final HttpGet get = new HttpGet("http://ec2-3-21-232-61.us-east-2.compute.amazonaws.com:8081/bookings/findByDate/" + day + "/" + month + "/" + year);
+
             String header = "Basic ";
             String headerValue = username + ":" + password;
             String encodedHeaderValue = Base64.encodeBase64String(headerValue.getBytes());
-            String headerBasic =  header + encodedHeaderValue;
-
-
-
+            String headerBasic = header + encodedHeaderValue;
             get.addHeader(HttpHeaders.AUTHORIZATION, headerBasic);
 
             // En response handler taget fra Apaches side: https://hc.apache.org/httpcomponents-client-5.0.x/httpclient5/examples/ClientWithResponseHandler.java
@@ -190,11 +183,10 @@ public class RestApiClient {
             String respnsNew5 = respnsNew4.replace("]", "");
             String respnsNew6 = respnsNew5.replace(" ", " - ");
             return respnsNew6;
-
         }
     }
 
-    public static void createBooking(int timeblock, int roomId, String userid, String username, int month, int year, int day,String userName, String password ) throws IOException, JSONException {
+    public static void createBooking(int timeblock, int roomId, String userid, String username, int month, int year, int day, String userName, String password) throws IOException, JSONException {
         try (final CloseableHttpClient httpclient = HttpClients.createDefault()) {
             final HttpPost post = new HttpPost("http://ec2-3-21-232-61.us-east-2.compute.amazonaws.com:8081/bookings");
 
@@ -206,13 +198,12 @@ public class RestApiClient {
             payload.put("month", month);
             payload.put("year", year);
             payload.put("day", day);
-
             post.setEntity(new StringEntity(payload.toString(), ContentType.APPLICATION_JSON));
 
             String header = "Basic ";
             String headerValue = username + ":" + password;
             String encodedHeaderValue = Base64.encodeBase64String(headerValue.getBytes());
-            String headerBasic =  header + encodedHeaderValue;
+            String headerBasic = header + encodedHeaderValue;
             post.addHeader(HttpHeaders.AUTHORIZATION, headerBasic);
 
             // En response handler taget fra Apaches side: https://hc.apache.org/httpcomponents-client-5.0.x/httpclient5/examples/ClientWithResponseHandler.java
@@ -228,14 +219,11 @@ public class RestApiClient {
                 } else {
                     throw new ClientProtocolException("Unexpected response status: " + status + " Couldnt create booking");
                 }
-
             };
-
             final String responseBody = httpclient.execute(post, responseHandler);
             System.out.println("-------------------------");
-
-            }
         }
+    }
 
     public static String getUserid(String username, String password) throws IOException, JSONException, ParseException, URISyntaxException {
 
@@ -245,7 +233,7 @@ public class RestApiClient {
             String header = "Basic ";
             String headerValue = username + ":" + password;
             String encodedHeaderValue = Base64.encodeBase64String(headerValue.getBytes());
-            String headerBasic =  header + encodedHeaderValue;
+            String headerBasic = header + encodedHeaderValue;
             get.addHeader(HttpHeaders.AUTHORIZATION, headerBasic);
 
             // En response handler taget fra Apaches side: https://hc.apache.org/httpcomponents-client-5.0.x/httpclient5/examples/ClientWithResponseHandler.java
@@ -264,9 +252,7 @@ public class RestApiClient {
 
             };
             final String responseBody = httpclient.execute(get, responseHandler);
-
             CloseableHttpResponse result = httpclient.execute(get);
-
             HttpEntity entity = result.getEntity();
             String content = EntityUtils.toString(entity);
             JSONObject myObject = new JSONObject(content);
@@ -274,14 +260,14 @@ public class RestApiClient {
         }
     }
 
-    public static String getBookings(String userid,String username, String password) throws IOException, JSONException, ParseException {
+    public static String getBookings(String userid, String username, String password) throws IOException, JSONException, ParseException {
 
         try (final CloseableHttpClient httpclient = HttpClients.createDefault()) {
             final HttpGet get = new HttpGet("http://ec2-3-21-232-61.us-east-2.compute.amazonaws.com:8081/bookings/user/" + userid);
             String header = "Basic ";
             String headerValue = username + ":" + password;
             String encodedHeaderValue = Base64.encodeBase64String(headerValue.getBytes());
-            String headerBasic =  header + encodedHeaderValue;
+            String headerBasic = header + encodedHeaderValue;
             get.addHeader(HttpHeaders.AUTHORIZATION, headerBasic);
 
             // En response handler taget fra Apaches side: https://hc.apache.org/httpcomponents-client-5.0.x/httpclient5/examples/ClientWithResponseHandler.java
@@ -299,9 +285,7 @@ public class RestApiClient {
                 }
 
             };
-
             final String responseBody = httpclient.execute(get, responseHandler);
-
             String responseNew = responseBody.replace("[", "");
             String responseNew1 = responseNew.replace("{", "|");
             String responseNew2 = responseNew1.replace("}", "|" + "\n");
@@ -312,6 +296,5 @@ public class RestApiClient {
             return respnsNew6;
         }
     }
-
 }
 
